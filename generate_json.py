d3329be62dcb03b56efdd7b9e25597d0e04787e4
Dave <fatdavemakes@gmail.com>
2023-07-20 20:19:34 +0100

    Update python script to handle mangrove and bamboo + add cherry
    Adds Mangrove
    Adds bamboo
    	Bamboo has no wood variant so adds filter option into tuple
    	and a filter on the template handling
    Adds Cherry

--- generate_json.py
+++ generate_json.py
@@ -4,16 +4,16 @@ WORD_TO_REPLACE = "WOOD"
 WORD_TO_REPLACE_LOG = "WOOD_log"
 WORD_TO_REPLACE_STRIPPED_WOOD = "WOOD_wood"
 WORD_TO_REPLACE_SAPLING = "WOOD_sapling"
-TEMPLATES_DIR_RELATIVE = "\\templates"
-OUTPUT_DIR_RELATIVE = "\\data\\woodcutter\\recipes"
+TEMPLATES_DIR_RELATIVE = "templates"
+OUTPUT_DIR_RELATIVE = os.path.join("data", "woodcutter", "recipes")
 #ADVANCEMENT_DIR_RELATIVE = "\\data\\woodcutter\\advancements\\recipes\\misc"
-woods = ['oak', 'spruce', 'birch', 'dark_oak', 'acacia', 'jungle', ('warped', 'warped_stem', 'warped_hyphae', 'warped_fungus'), ('crimson', 'crimson_stem', 'crimson_hyphae', 'crimson_fungus')]
-# format for each wood item is ('<plank/generic>', <optional_log_and_stripped_override>, <optional_stripped_wood_override>, <optional_sapling_override>)
+woods = ['oak', 'spruce', 'birch', 'dark_oak', 'acacia', 'jungle', 'mangrove', 'cherry', ('mangrove', 'mangrove_log', 'mangrove_wood', 'mangrove_propagule', True), ('bamboo', 'bamboo_block', 'bamboo_block', 'bamboo', False), ('warped', 'warped_stem', 'warped_hyphae', 'warped_fungus', True), ('crimson', 'crimson_stem', 'crimson_hyphae', 'crimson_fungus', True)]
+# format for each wood item is ('<plank/generic>', <optional_log_and_stripped_override>, <optional_stripped_wood_override>, <optional_sapling_override>, <has wood type>)
 # if specifying any optional fields, you must specify them all, so that order is maintained
 
 currentDir = os.path.dirname(os.path.abspath(__file__))
-templatesDir = currentDir + TEMPLATES_DIR_RELATIVE
-outputDir = currentDir + OUTPUT_DIR_RELATIVE
+templatesDir = os.path.join(currentDir,TEMPLATES_DIR_RELATIVE)
+outputDir = os.path.join(currentDir, OUTPUT_DIR_RELATIVE)
 #advanceDir = currentDir + ADVANCEMENT_DIR_RELATIVE
 print("Reading templates from " + templatesDir + "\nOutputting to " + outputDir)
 templateFiles = os.listdir(templatesDir)
@@ -33,15 +33,14 @@ if(not(os.path.exists(outputDir))):
 
 for file in templateFiles:
         if ".json" in file:
-                print('Processing recipe template ' + file )
+                print('\nProcessing recipe template ' + file )
                 newLines = []
-                with open(templatesDir + "\\" + file, "r") as f:
+                with open(os.path.join(templatesDir, file), "r") as f:
                         #read_data = f.read()
                         for line in f:
                                 newLines.append(line)
                                 #print(line, end='')
                 #print(newLines)
-
                 for variant in woods:
                         has_overrides = type(variant) is tuple
 
@@ -49,12 +48,19 @@ for file in templateFiles:
                         log_and_stripped_override_name = variant[1] if has_overrides else None
                         stripped_wood_override_name = variant[2] if has_overrides else None
                         sapling_override_name = variant[3] if has_overrides else None
-
+                        has_wood_type = variant[4] if has_overrides else True
+                        
+                        
                         print("\t" + primary_name + " (has overrides?: " + str(has_overrides) + ")")
+                        if "_wood" in file and not has_wood_type:
+                            break # We don't do wood variants if the wood type(bamboo) doesn't have one
+                        if "stripped_wood" in file and not has_wood_type:
+                            break # We don't do stripped wood variants if the wood type(bamboo) doesn't have one
+
                         newFileName = file.replace(WORD_TO_REPLACE, primary_name)
                         if "boat" in newFileName and has_overrides:
                             break # override woods don't have boat variants, so don't generate boat related recipes for them
-                        with open(outputDir + "\\" + newFileName, "w+") as newfile:
+                        with open(os.path.join(outputDir,newFileName), "w+") as newfile:
                                 for line in newLines:
                                         resultLine = line
                                         if has_overrides: # first, replace the more specific lines, then the general ones
